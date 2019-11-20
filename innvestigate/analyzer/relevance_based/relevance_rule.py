c0e778dc894e8bfe33935ed38626ae5cc4bc6680
Tomas Pohl <tomas.pohl@yahoo.com>
2019-11-20 12:42:42 +0100

    Debug

--- innvestigate/analyzer/relevance_based/relevance_rule.py
+++ innvestigate/analyzer/relevance_based/relevance_rule.py
@@ -358,7 +358,8 @@ class AlphaBetaRule(kgraph.ReverseMappingBase):
                         for a, b in zip(activator_relevances, inhibitor_relevances)]
         elif self._activators_sqrt and not self._beta: #if wanna compute the square root of the activators (but only if beta is zero)
             print('Applying square root to positive contributions...(also tensor!)')
-            return [K.sqrt(K.constant(np.array(activator_relevances)))]
+            print('activator_relevances.length', len(activator_relevances))
+            return [K.sqrt(K.constant(activator_relevances))]
         else:
             return activator_relevances
 
