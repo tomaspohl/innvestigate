82827b8658e44473552dc7fbd89be0b30866a8d6
Tomas Pohl <tomas.pohl@yahoo.com>
2019-11-20 23:34:31 +0100

    Fix Flat Square rule

--- innvestigate/analyzer/relevance_based/relevance_rule.py
+++ innvestigate/analyzer/relevance_based/relevance_rule.py
@@ -298,7 +298,7 @@ class FlatSquareRule(FlatRule):
         # Redistribute the relevances along the gradient.
         g = grad(Xs+Ys+tmp)
         # Apply square()
-        g_square = [x ** 2 for x in g]
+        g_square = g ** 2
         tmp = iutils.to_list(g_square)
 
         return tmp
